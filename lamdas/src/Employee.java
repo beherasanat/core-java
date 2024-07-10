6fca9d8df58f495c3dcff16e3926685f21ff30a9
sanat kumar behera <behera.sanat@gmail.com>
2024-07-10 17:59:05 +0530

    summary of stream intermediate and terminal functions.

---
+++ lamdas/src/Employee.java
@@ -0,0 +1,43 @@
+public class Employee {
+    String name;
+    double salary;
+    int id;
+
+    public Employee() {
+
+    }
+    public Employee(String name, double salary, int id) {
+        this.name = name;
+        this.salary = salary;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "{name=" + name + ", salary=" + salary + ", id=" + id + "}";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ lamdas/src/StreamIntermediateTerminalMethods.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamIntermediateTerminalMethods {
+    public void m() {
+        List list = new ArrayList();
+
+        // TERMINAL OPERATORS
+
+        /*list.stream().forEach();
+        list.stream().forEachOrdered();
+        list.stream().reduce();
+        list.stream().collect();
+        list.stream().min();
+        list.stream().max();
+        list.stream().anyMatch();
+        list.stream().findAny();
+        list.stream().findFirst();
+        list.stream().noneMatch();
+        list.stream().count();
+        list.stream().allMatch();
+        list.stream().isParallel();
+        list.stream().toArray();
+        list.stream().toList();*/
+
+
+        // INTERMEDIATE OPERATORS
+
+        /*list.stream().peek();
+        list.stream().takeWhile();
+        list.stream().skip();
+        list.stream().limit();
+        list.stream().mapMulti();
+        list.stream().dropWhile();
+        list.stream().filter();
+        list.stream().sorted();
+        list.stream().parallel();
+        list.stream().distinct();
+        list.stream().flatMap();
+        list.stream().map();*/
+
+    }
+}
